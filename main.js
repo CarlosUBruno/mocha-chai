f4912d2bda260e7f25819e813036fb178f690512
CarlosUBruno <cbruno@frro.utn.edu.ar>
2025-08-17 17:20:56 -0300

    Se agrega el ejemplo de una funcion asincronica, after y before each

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function sumaEnteros(a, b) {
   return a + b;
 }
-
+function getNumeroAsync() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(42), 500); // simula demora
+  });
+}
 
 module.exports = {
-    sumaEnteros
+    sumaEnteros,
+    getNumeroAsync
 };
-
--- test/main.test.js
+++ test/main.test.js
@@ -3,6 +3,26 @@ const main = require('../main');
 //resultados
 
 describe('Pruebas de sumaEnteros', function() {
+  before(function() {
+    // Se ejecuta UNA VEZ antes de todos los tests
+    console.log('Antes de todos los tests');
+  });
+
+  after(function() {
+    // Se ejecuta UNA VEZ después de todos los tests
+    console.log('Después de todos los tests');
+  });
+
+  beforeEach(function() {
+    // Se ejecuta ANTES de cada test
+    console.log('Antes de cada test');
+  });
+
+  afterEach(function() {
+    // Se ejecuta DESPUÉS de cada test
+    console.log('Después de cada test');
+  });
+
   it('Debe retornar 8 cuando sumo 6 + 2', function() {
     assert.strictEqual(main.sumaEnteros(6, 2), 8);
   });
@@ -10,4 +30,13 @@ describe('Pruebas de sumaEnteros', function() {
   it('Debe retornar -4 cuando sumo -2 + -2', function() {
     assert.strictEqual(main.sumaEnteros(-2, -2), -4);
   });
+
+  it('Debe retornar 42 de manera asincrónica', async function() {
+    const resultado = await main.getNumeroAsync();
+    assert.strictEqual(resultado, 42);
+  });
 });
+/* Mocha muestra primero los test asincrónicos y luego los sincrónicos
+tambien resalta en rojo los test que tardan mas de 75ms (por defecto)
+para advertir que son "lentos", que el test tarda más de lo esperado.
+*/
